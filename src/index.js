32d9958426432e2109d531b8635b22bb2bd4e3cb
yv <yavictor37@yandex.ru>
2019-07-24 00:01:35 +0300

    engine logic fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { car, cdr } from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
 
-
 const askPlayerName = () => readlineSync.question('May I have your name? ');
 
 const askAnswer = () => readlineSync.question('Your answer: ');
@@ -13,23 +12,22 @@ const isEven = num => ((num % 2 === 0) ? 'yes' : 'no');
 const gameEngine = (gameDescr, gameCons) => {
   console.log('Welcome to the Brain Games');
   console.log(gameDescr);
-  const playerName = askPlayerName;
+  const playerName = askPlayerName();
   console.log(`Hello ${playerName}! \n`);
 
-  for (let i = 0; i < 4; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const constructor = gameCons();
     const askQuestion = car(constructor);
     const corAnswer = cdr(constructor);
     console.log(`Question: ${askQuestion}`);
     const playerAnswer = askAnswer();
-    // console.log(`${String(corAnswer)} === ${String(playerAnswer)}`);
-    if (String(corAnswer) === String(playerAnswer) && i === 3) {
-      console.log(`Congratulations, ${playerName}!`);
-    }
     if (String(corAnswer) === String(playerAnswer)) {
       console.log('Correct');
-    } else {
-      i = 4;
+      if (String(corAnswer) === String(playerAnswer) && i >= 2) {
+        console.log(`Congratulations, ${playerName}!`);
+      }
+    } else if (String(corAnswer) !== String(playerAnswer)) {
+      i = 3;
       console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${corAnswer}. \r
   Let's try again, ${playerName}!`);
     }
