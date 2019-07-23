14426b3aeefac8cc56475ddcdb7c32334c675f18
yv <yavictor37@yandex.ru>
2019-07-23 23:18:15 +0300

    game engine fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import askPlayerName from '..';
+import { askPlayerName } from '..';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games! \n');
 const playerName = askPlayerName();
 console.log(`Hello, ${playerName}!`);
--- src/index.js
+++ src/index.js
@@ -11,30 +11,31 @@ const randomNumber = maxNum => Math.floor(Math.random() * (maxNum - 1));
 const isEven = num => ((num % 2 === 0) ? 'yes' : 'no');
 
 const gameEngine = (gameDescr, gameCons) => {
-  console.log('Welcome to Brain Games');
+  console.log('Welcome to the Brain Games');
   console.log(gameDescr);
-  const playerName = askPlayerName();
+  const playerName = askPlayerName;
   console.log(`Hello ${playerName}! \n`);
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 4; i += 1) {
     const constructor = gameCons();
     const askQuestion = car(constructor);
     const corAnswer = cdr(constructor);
     console.log(`Question: ${askQuestion}`);
     const playerAnswer = askAnswer();
-    if (+corAnswer === +playerAnswer) {
+    // console.log(`${String(corAnswer)} === ${String(playerAnswer)}`);
+    if (String(corAnswer) === String(playerAnswer) && i === 3) {
+      console.log(`Congratulations, ${playerName}!`);
+    }
+    if (String(corAnswer) === String(playerAnswer)) {
       console.log('Correct');
     } else {
-      i = 3;
+      i = 4;
       console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${corAnswer}. \r
   Let's try again, ${playerName}!`);
     }
   }
-  console.log(`Congratulations, ${playerName}`);
 };
 
 export default gameEngine;
 
-export {
-  isEven, randomNumber,
-};
+export { askPlayerName, isEven, randomNumber };
