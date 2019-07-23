ca807aea35ae9c98849545b44ba8a64e6b646626
yv <yavictor37@yandex.ru>
2019-07-23 20:45:04 +0300

    Rewrite game-engine in outer module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
+import { car, cdr, cons } from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
+
 
 const askPlayerName = () => readlineSync.question('May I have your name? ');
 
 const askAnswer = () => readlineSync.question('Your answer: ');
 
-const randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+const randomNumber = maxNum => Math.floor(Math.random() * (maxNum - 1)) + 1;
 
-export default askPlayerName;
+const isEven = num => ((num % 2 === 0) ? 'yes' : 'no');
 
-export { askAnswer, randomNumber };
+const gameEngine = (gameDescr, gameCons) => {
+  console.log('Welcome to Brain Games');
+  console.log(gameDescr);
+  const playerName = askPlayerName();
+  console.log(`Hello ${playerName}! \n`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const constructor = gameCons();
+    const askQuestion = car(constructor);
+    const corAnswer = cdr(constructor);
+    console.log(`Question: ${askQuestion}`);
+    const playerAnswer = askAnswer();
+    if (corAnswer === playerAnswer) {
+      console.log('Correct');
+    } else {
+      i = 3;
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${corAnswer}. \r
+  Let's try again, ${playerName}!`);
+    }
+  }
+  console.log(`Congratulations, ${playerName}`);
+};
+
+export default gameEngine;
+
+export {
+  isEven, randomNumber,
+};
