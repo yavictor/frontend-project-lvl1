0fa749c866565ddbb456cf47e30cda449039f29f
yv <yavictor37@yandex.ru>
2019-07-30 12:24:13 +0300

    Change function name and replace it in upper scope

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,18 +4,19 @@ import gameEngine, { randomNumber } from '..';
 
 const description = 'Find the greatest common divisor of given numbers. \n';
 
+const gcdSearch = (num1, num2) => {
+  if (num2 > num1) {
+    return gcdSearch(num2, num1);
+  } if (num2 === 0) {
+    return num1;
+  } return gcdSearch(num2, (num1 % num2));
+};
+
 const gcdGame = () => {
   const number1 = randomNumber(1, 101);
   const number2 = randomNumber(1, 101);
   const gcdNumbers = `${number1} ${number2}`;
-  const gcdOfNumbers = (num1, num2) => {
-    if (num2 > num1) {
-      return gcdOfNumbers(num2, num1);
-    } if (num2 === 0) {
-      return num1;
-    } return gcdOfNumbers(num2, (num1 % num2));
-  };
-  const correctAnswer = gcdOfNumbers(number1, number2);
+  const correctAnswer = gcdSearch(number1, number2);
 
   return cons(gcdNumbers, correctAnswer);
 };
