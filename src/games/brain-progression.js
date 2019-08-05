82a7a4b6ac097d6d34d364ecc8b1854fb0f87d7a
yv <yavictor37@yandex.ru>
2019-08-05 13:34:50 +0300

    Renaming constants and optimizing algorithm in brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,22 +3,24 @@ import gameEngine, { randomNumber } from '..';
 
 const description = 'What number is missing in the progression?';
 
+const progressionSize = 10;
+
 const progressionGame = () => {
-  const number1 = randomNumber(1, 30);
+  const startNumber = randomNumber(1, 30);
   const progressionStep = randomNumber(2, 6);
-  const missingNumberPosition = randomNumber(0, 9);
-  const createProgression = (from, progressStep, missingNumber) => {
+  const missingNumberPosition = randomNumber(0, progressionSize - 1);
+  const createProgression = (progressionStart, progressStep, missingNumber) => {
     let progressionLine = '';
-    const progressionLimit = progressStep * 10 + from;
-    for (let i = from; i < progressionLimit; i += progressionStep) {
-      if (i === missingNumber * progressStep + from) {
+    const progressionLimit = progressStep * progressionSize + progressionStart;
+    for (let i = progressionStart; i < progressionLimit; i += progressionStep) {
+      if (i === missingNumber * progressStep + progressionStart) {
         progressionLine += '.. ';
       } else progressionLine += `${i} `;
     }
     return progressionLine;
   };
-  const correctAnswer = missingNumberPosition * progressionStep + number1;
-  const runProgression = createProgression(number1, progressionStep, missingNumberPosition);
+  const correctAnswer = missingNumberPosition * progressionStep + startNumber;
+  const runProgression = createProgression(progressionStart, progressionStep, missingNumberPosition);
   return cons(runProgression, correctAnswer);
 };
 
