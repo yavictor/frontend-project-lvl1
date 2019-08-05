93d0f2c9715cc13d22c3e0e2eef9e13f4edea745
yv <yavictor37@yandex.ru>
2019-08-05 12:10:26 +0300

    Removed constant from function in brain-calc module

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,11 +3,12 @@ import gameEngine, { randomNumber } from '..';
 
 const description = 'What is the result of the expression? \n';
 
+const number1 = randomNumber(1, 30);
+const number2 = randomNumber(1, 10);
+const operations = '+-*';
+const expression = mathOperations.charAt(randomNumber(1, 3));
+
 const calcGame = () => {
-  const number1 = randomNumber(1, 30);
-  const number2 = randomNumber(1, 10);
-  const mathOperations = '+-*';
-  const expression = mathOperations.charAt(randomNumber(1, 3));
   const randomExpression = `${number1} ${expression} ${number2}`;
   const randomExpressionResult = (x, y, expressionType) => {
     switch (expressionType) {
