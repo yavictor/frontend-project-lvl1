e22e72e82a4557bdf555de607dcce71ee1d3c201
yv <yavictor37@yandex.ru>
2019-08-07 12:54:44 +0300

    Return random generators inside function, change main function name

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,26 +4,27 @@ import randomNumber from '../utils';
 
 const description = 'What is the result of the expression?';
 
-const number1 = randomNumber(1, 30);
-const number2 = randomNumber(1, 10);
 const mathOperations = '+-*';
-const randomOperation = mathOperations.charAt(randomNumber(1, operations.length));
 
-const calcGame = () => {
-  const task = `${number1} ${randomOperation} ${number2}`;
-  const taskResult = (x, y, operation) => {
-    switch (operation) {
-      case '-':
-        return x - y;
-      case '+':
-        return x + y;
-      default:
-        return x * y;
-    }
-  };
-  const correctAnswer = taskResult(number1, number2, randomOperation);
-
-  return cons(task, correctAnswer);
+const taskResult = (x, y, operation) => {
+  switch (operation) {
+    case '-':
+      return x - y;
+    case '+':
+      return x + y;
+    default:
+      return x * y;
+  }
 };
 
-export default () => gameEngine(description, calcGame);
+const getCalcGame = () => {
+  const number1 = randomNumber(1, 30);
+  const number2 = randomNumber(1, 10);
+  const randomOperation = mathOperations.charAt(randomNumber(1, mathOperations.length));
+  const question = `${number1} ${randomOperation} ${number2}`;
+  const correctAnswer = taskResult(number1, number2, randomOperation);
+
+  return cons(question, correctAnswer);
+};
+
+export default () => gameEngine(description, getCalcGame);
