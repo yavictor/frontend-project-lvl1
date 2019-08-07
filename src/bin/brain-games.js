6b1e46a02010943bcdc469db140b36837503d68d
yv <yavictor37@yandex.ru>
2019-08-07 18:40:41 +0300

    Remove brain-game emty module

--- src/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games! \n');
-const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!`);
